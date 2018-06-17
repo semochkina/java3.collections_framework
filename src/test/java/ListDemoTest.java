6d0866118c2d79cfc4509d5d3b04482f58de224c
semochkina <36768972+semochkina@users.noreply.github.com>
2018-06-17 22:10:35 +0600

    changes

--- src/test/java/ListDemoTest.java
+++ src/test/java/ListDemoTest.java
@@ -1,10 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ListDemoTest {
     private ListDemo listDemo = new ListDemo();
@@ -39,16 +38,16 @@ public class ListDemoTest {
 
     @Test
     public void notIntersectTest() {
-        int[] array = {1, 2, 3};
-        List<int[]> ints = listDemo.notIntersect(Arrays.asList(
-                new int[]{1, 3, 5},
-                new int[]{2, 4, 6},
-                new int[]{3, 5, 7},
-                new int[]{4, 6, 8}), array);
+        Set<Integer> array = Stream.of(1, 2, 3).collect(Collectors.toSet());
+        List<Set<Integer>> ints = listDemo.notIntersect(Arrays.asList(
+                new HashSet(Arrays.asList(1, 3, 5)),
+                new HashSet(Arrays.asList(2, 4, 6)),
+                new HashSet(Arrays.asList(3, 5, 7)),
+                new HashSet(Arrays.asList(4, 6, 8))), array);
         Assert.assertEquals(ints.size(), 1);
-        Assert.assertEquals(ints.get(0)[0], 4);
-        Assert.assertEquals(ints.get(0)[1], 6);
-        Assert.assertEquals(ints.get(0)[2], 8);
+        Assert.assertTrue(ints.get(0).contains(4));
+        Assert.assertTrue(ints.get(0).contains(6));
+        Assert.assertTrue(ints.get(0).contains(8));
     }
 
     @Test
@@ -67,11 +66,10 @@ public class ListDemoTest {
         map.put(3, human3);
         map.put(4, human4);
         map.put(5, human5);
-        final List<Human> surnameEqual = listDemo.getHumans(map, new int[] {1});
+        final List<Human> surnameEqual = listDemo.getHumans(map, Collections.singleton(1));
         Assert.assertEquals(surnameEqual.size(), 1);
         Assert.assertEquals(surnameEqual.get(0).getAge(), 11);
     }
-
 
     @Test
     public void getHumansNotYoungTest() {
@@ -81,7 +79,7 @@ public class ListDemoTest {
         map.put(3, human3);
         map.put(4, human4);
         map.put(5, human5);
-        final List<Human> surnameEqual = listDemo.getHumansNotYoung(map, new int[] {1, 2, 3, 4, 5});
+        final List<Human> surnameEqual = listDemo.getHumansNotYoung(map, new HashSet(Arrays.asList(1, 2, 3, 4, 5)));
         Assert.assertEquals(surnameEqual.size(), 4);
     }
 
